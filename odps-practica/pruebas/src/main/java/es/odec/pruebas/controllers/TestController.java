155dd951be37070144e33fb81bf73d8074b4f5c5
odps <odps31@gmail.com>
2025-03-18 11:18:19 +0100

    Pruebas con JPA para obtener resultados desde paquete PLSQL

---
+++ odps-practica/pruebas/src/main/java/es/odec/pruebas/controllers/TestController.java
@@ -0,0 +1,25 @@
+package es.odec.pruebas.controllers;
+
+import es.odec.pruebas.models.User;
+import es.odec.pruebas.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    UserRepo userRepo;
+
+    @GetMapping("/userid/{id}")
+    ResponseEntity<?> getUserById(@PathVariable int id) {
+        User user = userRepo.findUserByIdQuery(id);
+        System.out.println(user.toString());
+        return ResponseEntity.ok(user);
+    }
+}
--- odps-practica/pruebas/src/main/java/es/odec/pruebas/repositories/UserRepo.java
+++ odps-practica/pruebas/src/main/java/es/odec/pruebas/repositories/UserRepo.java
@@ -1,12 +1,34 @@
 package es.odec.pruebas.repositories;
 
 import es.odec.pruebas.models.User;
-import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Integer> , JpaSpecificationExecutor<User> {
+public interface UserRepo extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+
     User findByUsername(String username);
+
+    //@Query(value = "CALL op_paquete2.obtenerusuario(:id)", nativeQuery = true)
+    @Query(value = "SELECT op_paquete2.obtenerusuario(:id) from DUAL", nativeQuery = true)
+    User findUserByIdQuery(@Param("id") int id);
+
+
+    // Call a function from your package that returns a single value
+//    @Query(value = "SELECT YOUR_PACKAGE_NAME.YOUR_FUNCTION_NAME(:productId) FROM dual", nativeQuery = true)
+//    String callCustomFunction(@Param("productId") int productId);
+//
+//    // Call a function that returns a cursor which you want to map to entities
+//    @Query(value = "SELECT * FROM TABLE(YOUR_PACKAGE_NAME.GET_PRODUCTS_FUNCTION(:category))", nativeQuery = true)
+//    List<Product> getProductsByCategory(@Param("category") String category);
+//
+//    // To execute a procedure, you can use the @Modifying annotation
+//    @Modifying
+//    @Query(value = "BEGIN YOUR_PACKAGE_NAME.YOUR_PROCEDURE_NAME(:param1, :param2); END;", nativeQuery = true)
+//    void executePackageProcedure(@Param("param1") int param1, @Param("param2") String param2);
+
+
 }
